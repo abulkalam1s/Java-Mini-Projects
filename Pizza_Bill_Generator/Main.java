06fefbb982b1d7a6daa169df7541217cc82294d5
abulkalam1s <abulakalamjmt3@gmail.com>
2024-06-15 21:52:58 +0530

    updated

--- Pizza_Bill_Generator/Main.java
+++ Pizza_Bill_Generator/Main.java
@@ -1,16 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         
-        // Pizza basePizza = new Pizza(false);
+        Pizza basePizza = new Pizza(false);
 
-        // basePizza.addExtraCheese();
-        // basePizza.addExtraToppings();
-        // basePizza.takeAway();
+        basePizza.addExtraCheese();
+        basePizza.addExtraToppings();
+        basePizza.takeAway();
 
-        // basePizza.generateBill();  // Final bill after additions
+        basePizza.generateBill();  // Final bill after additions
 
-        DeluxPizza dp = new DeluxPizza(false);
-        dp.takeAway();
-        dp.generateBill();
+        // DeluxPizza dp = new DeluxPizza(false);
+        // dp.takeAway();
+        // dp.generateBill();
+
     }
 }
